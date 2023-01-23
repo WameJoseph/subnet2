454abdfa8f5a54b7998d4d5be560e4ee25ddcec9
WameJoseph <101824766+WameJoseph@users.noreply.github.com>
2023-01-23 15:48:55 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -11,3 +11,48 @@ function sidebarOff(){
     document.getElementById("i").style.display = "flex";
     // document.body.style.overflow = "scroll";
 }
+
+
+function section1on(){
+    document.getElementById("section1").style.display = "flex";
+    document.getElementById("section2").style.display = "none";
+    document.getElementById("section3").style.display = "none";
+    document.getElementById("section4").style.display = "none";
+}
+
+function section2on(){
+    document.getElementById("section2").style.display = "flex";
+    document.getElementById("section1").style.display = "none";
+    document.getElementById("section3").style.display = "none";
+    document.getElementById("section4").style.display = "none";
+}
+
+function section3on(){
+    document.getElementById("section3").style.display = "flex";
+    document.getElementById("section1").style.display = "none";
+    document.getElementById("section2").style.display = "none";
+    document.getElementById("section4").style.display = "none";
+}
+
+function section4on(){
+    document.getElementById("section4").style.display = "flex";
+    document.getElementById("section2").style.display = "none";
+    document.getElementById("section3").style.display = "none";
+    document.getElementById("section1").style.display = "none";
+}
+
+// if (document.getElementById("section1").style.display = "flex"){
+//     document.getElementById("sc_1").style.backgroundColor = "white";
+// }
+
+// if (document.getElementById("section2").style.display = "flex"){
+//     document.getElementById("sc_2").style.backgroundColor = "blue";
+// }
+
+// if (document.getElementById("section3").style.display = "flex"){
+//     document.getElementById("sc_3").style.backgroundColor = "blue";
+// }
+
+// if (document.getElementById("section4").style.display = "flex"){
+//     document.getElementById("sc_4").style.backgroundColor = "blue";
+// }
